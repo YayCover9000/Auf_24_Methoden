4b5dbdbc0a98e0a2bf9b4994508d92231fc267b9
YayCover9000 <yassuanf@googlemail.com>
2022-12-20 23:01:16 +0100

    unfertig mus noch was testen vorher

---
+++ src/Auf_24_Methoden.java
@@ -0,0 +1,33 @@
+
+public class Auf_24_Methoden {
+    public static int berechnungZiffernsumme() {
+        int zaehler;
+        int ziffernsumme = 0; // Initialisierung mit 0 für Rückgabe im
+        // Fehlerfall und bei Addition wichtig!
+
+
+        // Eingabe
+        System.out.println("Bitte geben Sie für die Berechnung der Zifferensumme eine ganze positive Zahl ein: ");
+
+        zaehler = eingabeZahl;
+
+        // Berechnung der Ziffernsumme
+        while (zaehler > 0) { // Beinhalte die Prüfung, dass Zahl der Eingabe positiv ist
+            ziffernsumme = ziffernsumme + (zaehler % 10); // Einerstelle zur Summe
+            // addierenn
+            // Es kann auch bei Integer.MAX_VALUE nicht zum Überlauf
+            // kommen
+            zaehler = zaehler / 10; // Einerstelle abschneiden
+
+        }
+
+        // Ausgabe
+        if (ziffernsumme > 0) { // Ziffernsumme muss nach einer Berechnung groeßer 0 sein.
+            System.out.println("Das Ergebnis zur Berechnung der Ziffernsumme von " + eingabeZahl + " lautet "
+                    + ziffernsumme + ".");
+        } else {
+            System.out.println("Sie haben einen falschen Wert eingegeben.");
+        }
+        return ziffernsumme;
+    }
+}
---
+++ src/Testtreiber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Testtreiber {
+
+    public static int main(int ziffersumme){
+        Scanner sc = new Scanner(System.in);
+        int eingabeZahl = 0;
+        int test;
+        eingabeZahl = sc.nextInt();
+       test = Auf_24_Methoden.berechnungZiffernsumme()
+        sc.close();
+        return eingabeZahl;
+    }
+
+}
